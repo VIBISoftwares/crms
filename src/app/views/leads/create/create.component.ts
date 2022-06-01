dcbcc367634eb00ce1587683b854562667047c68
Vinothkumar <kaspondevelopers@gmail.com>
2022-06-01 19:00:04 +0530

    lead screen updates

--- src/app/views/leads/create/create.component.ts
+++ src/app/views/leads/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { createAutoCorrectedDatePipe } from 'text-mask-addons';
 
 @Component({
@@ -9,15 +9,39 @@ import { createAutoCorrectedDatePipe } from 'text-mask-addons';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  guardian_type:any;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
+  title = ['Mr', 'Mrs', 'Miss', 'Dr'];
+  maritial_staus = ['Single', 'Married', 'Divorce', 'Seperater'];
+  guardian = [
+    'Father',
+    'Mother',
+    'Son',
+    'Daughter',
+    'grandFather',
+    'GrandSon',
+    'Guardian',
+  ];
+  occupation = ['Engineer', 'Doctor', 'Self Business', 'Agriculture'];
+  country = ['India', 'Singapore', 'Malaisiya'];
+  state = ['Tamilnadu', 'AndraPradesh', 'Telugana', 'Kerala', 'Karnadaka'];
+  district = [
+    'Chennai',
+    'Chengalpattu',
+    'Thiruvallur',
+    'Kanjipuram',
+    'Villupuram',
+    'Trichy',
+    'Madurai',
+    'Selam',
+  ];
+  area=['Adaiyar','Vadapalani','Tharamani','Thambaram'];
+  source = ['Facebook','Instagram','Whatsapp','Twitter', 'LinkedIn']
   minDate = new Date(2017, 5, 10);
   maxDate = new Date(2022, 9, 15);
 
   bsValue: Date = new Date();
   bsRangeValue: any = [new Date(2020, 7, 4), new Date(2020, 7, 20)];
-  
-
 }
